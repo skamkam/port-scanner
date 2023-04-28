e639cfd728c613487dcc9715e3532c16d7afedf6
Sarah Kam <sarahskam@gmail.com>
2023-04-28 14:43:19 -0400

    working on syn scan

--- port_generation.py
+++ port_generation.py
@@ -10,12 +10,11 @@ import random
 def port_gen(subset, order):
     if subset:
         if order:
-            return (x for x in range(1024))
+            return [x for x in range(1024)]
         else: # return list of randomly ordered 0-1023
-            return random.sample(range(1024), k=1024)
+            return [random.sample(range(1024), k=1024)]
     else:
         if order:
-            return (x for x in range(65536))
+            return [x for x in range(65536)]
         else: # return list of randomly ordered 0-65536
-            return random.sample(range(65536), k=65536)
-
+            return [random.sample(range(65536), k=65536)]
--- port_scanner.py
+++ port_scanner.py
@@ -2,11 +2,24 @@
 # Smith College CSC251 Final Project: Port Scanner
 # Test server: glasgow.smith.edu, IP: 131.229.72.13
 # References:
+#       https://scapy.readthedocs.io/en/latest/usage.html#syn-scans
 
 import socket
+from scapy.all import *
 from port_generation import port_gen
 
+DST_IP = "131.229.72.13"
 
+# depending on options, port_gen provides a certain list
+#test_ports = port_gen(subset=True, order=True) # list of ports in order 0-1023
+test_ports = [80]
 
+for i in range(len(test_ports)):
+    # SYN scan all ports, ans is answered
+    ans, unans = sr1( IP(dst=DST_IP)/TCP(dport = test_ports[i] ,flags="S") ) # SYN scan all ports
+    ans.summary(lfilter = lambda s,r: r.sprintf("%TCP.flags%") == "SA", prn=lambda s,r: r.sprintf("%TCP.sport% open"))
+    #ans.summary( lambda s,r: r.sprintf("%TCP.sport% \t %TCP.flags%") )
+
+print("done")
 
 
